313fe54e39915bc9af07d557bb9889b251279c51
YRStock <yrstock@MacBook-Pro-21.local>
2024-03-10 17:54:08 +0100

    pynput keyboard listener stable version

--- OSC_Generator.py
+++ OSC_Generator.py
@@ -1,6 +1,6 @@
 import time
 import mido
-import keyboard
+from pynput import keyboard
 from pythonosc.udp_client import SimpleUDPClient
 
 
@@ -8,10 +8,10 @@ from pythonosc.udp_client import SimpleUDPClient
 ip = "127.0.0.1"
 port = 5000
 
+
 client = SimpleUDPClient(ip, port)  # Create client
 
-
-#initializing m-key flags
+#initializing midi-key flags
 
 #initialize command Nr
 
@@ -24,60 +24,81 @@ client.send_message("/some/address", [1, 2., "hello"])  # Send message with int,
 
 
 # Идентификатор инструмента для отслеживания (замените на нужное значение)
-TARGET_INSTRUMENT = 0
+# TARGET_INSTRUMENT = 0
+
+print("44444444444444")
 
 # Функция для обработки MIDI-сообщений
 def process_midi_message(message):
-    if message.type == 'note_on' and message.channel == TARGET_INSTRUMENT:
-        print(f"MIDI Note On: {message.note}")
-    elif message.type == 'note_off' and message.channel == TARGET_INSTRUMENT:
-        print(f"MIDI Note Off: {message.note}")
+    #if message.type == 'note_on' and message.channel == TARGET_INSTRUMENT:
+	if message.type == 'note_on':
+		print(f"MIDI Note On: {message.note}")
+	#elif message.type == 'note_off' and message.channel == TARGET_INSTRUMENT:
+		#print(f"MIDI Note Off: {message.note}")
     # Другие проверки, если необходимо
 
 # Функция для обработки событий клавиатуры
-def process_keyboard_event(event):
-    print(f"Keyboard: {event.name}")
+def on_key_press(key):
+    try:
+        print(f"Key {key.char} pressed")
+    except AttributeError:
+        print(f"Special key {key} pressed")
+
+
+
+print("555555555555555")
+
+#To get a list of available ports
+port_List = mido.get_output_names()
+print("MIDI-Ports available:", port_List)
+
+print("666666666666666")
 
 # Открываем виртуальный MIDI-вход для чтения сообщений
-input_port = mido.open_input('VirtualMIDISynth Port 1')  # Замените на имя вашего виртуального MIDI-устройства
+input_port = mido.open_input(port_List[0])  # Замените на имя вашего MIDI-устройства
 
 
-
-
-
-
+print("777777777777")
 
 
 #number of Milumin Column
 mn = 0
 
 
-#SENDING OSC MESSAGES
+print("8888888888888")
+
+with keyboard.Listener(on_press=on_key_press) as listener:
+#try:
+	print("99999999999")
 # Запускаем цикл для обработки онлайн MIDI-сообщений
-try:
-	for message in input_port:
-		process_midi_message(message)
+	while True:
+		message = input_port.poll()
+		if message:
+			process_midi_message(message)
+			mn += 1
+			msg = "/action/launchColumn"
+			#SENDING OSC MESSAGES
+			client.send_message(msg, mn) 
+			print("msg=",msg,"n=",mn)
+		#else:
+			#print("mn=",mn)
+			
+		
 
-		# Проверяем события клавиатуры при каждом MIDI-сообщении
-		if keyboard.is_pressed('q'):
-			print("Exit requested. Stopping...")
-			break
-        
-	mn += 1
-	msg = "/action/launchColumn"
-	client.send_message(msg, mn) 
-	print("msg=",msg,"n=",mn)
-	#time.sleep(3)
-        
-except KeyboardInterrupt :
-	pass     
+		time.sleep(0.1)
+'''       
+except KeyboardInterrupt:
+	pass    
+except:
+	print("Something else went wrong") 
+'''
 
-finally:
-	print ("\nClosing OSCClient")
+#finally:
+	#print ("\nClosing OSCClient")
 	#close OSC-port
-	client.close()
-	print ("Done. Closing MIDI-port")
-	input_port.close()  # Важно закрыть порт после использования
-	print ("\nMIDI-port closed")
+	#client.close() - there is no method close()
+print ("Closing MIDI-port")
+input_port.close()  # Важно закрыть порт после использования
+print ("\nMIDI-port closed")
 
 
